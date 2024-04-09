d40065d393e9ebc60486433eac6ba67bcc0f9fef
Juliana Battion <julibattion@gmail.com>
2024-04-09 16:20:56 -0300

    responsive 1

---
+++ js/main.js
@@ -0,0 +1,102 @@
+/* Nav */
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#boton-abrir");
+const cerrar = document.querySelector("#boton-cerrar");
+const enlacesSeccion = document.querySelectorAll("#nav ul li a");
+
+/* Carrusel */
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+
+/* Nav */
+
+abrir.addEventListener("click", () =>{
+    nav.classList.add("visible");
+    abrir.style.visibility = 'hidden';
+    cerrar.style.visibility = 'visible';
+});
+
+cerrar.addEventListener("click", () =>{
+    nav.classList.remove("visible");
+    abrir.style.visibility = 'visible';
+    cerrar.style.visibility = 'hidden';
+});
+
+enlacesSeccion.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        nav.classList.remove("visible");
+        abrir.style.visibility = 'visible';
+        cerrar.style.visibility = 'hidden';
+    });
+});
+
+
+/* Carrusel */
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+const autoPlay = () => {
+    if (!isAutoPlay) return;
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
---
+++ php/form.php
@@ -0,0 +1,35 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// Verifica si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recibe los datos del formulario
+    $nombre = $_POST["name"];
+    $correo = $_POST["mail"];
+    $asunto = $_POST["subject"];
+    $mensaje = $_POST["message"];
+    
+    // Dirección de correo electrónico a la que se enviará el formulario
+    $destinatario = "techrevive.informatica@gmail.com";
+    
+    // Construye el mensaje del correo electrónico
+    $contenido = "Nombre: $nombre\n";
+    $contenido .= "Correo electrónico: $correo\n";
+    $contenido .= "Asunto: $asunto\n";
+    $contenido .= "Mensaje:\n$mensaje\n";
+    
+    // Envía el correo electrónico
+    $resultado = mail($destinatario, "Nuevo mensaje del formulario", $contenido);
+    
+    // Verifica si el correo electrónico se envió correctamente
+    if ($resultado) {
+        echo "El mensaje ha sido enviado correctamente.";
+    } else {
+        echo "Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
+    }
+} else {
+    // Si no se ha enviado el formulario, redirige al formulario HTML
+    header("Location: formulario.html");
+}
+?>
